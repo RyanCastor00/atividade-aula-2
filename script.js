7befa3cca027ef77b84728a89a06e5c09bff8099
RyanCastor00 <ryanmatheussoaresdearaujo@gmail.com>
2026-03-10 10:23:28 -0300

    commit..

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-function somar(){
+function somar() {
     let num
     let num1
-    num = parseInt (document.getElementById('1').value)
-    num1 = parseInt (document.getElementById('2').value)
-    document.getElementById('resultado').innerText = num+num1
-    console.log(num+num1)
+    num = parseInt(document.getElementById('1').value)
+    num1 = parseInt(document.getElementById('2').value)
+    document.getElementById('resultado').innerText = num + num1
+    console.log(num + num1)
 }
-function subtrair(){
+function subtrair() {
     let num3
     let num4
-    num3 = parseInt (document.getElementById('3').value)
-    num4 = parseInt (document.getElementById('4').value)
-    document.getElementById('resultado2').innerText = num3-num4
-    console.log(num3-num4)
+    num3 = parseInt(document.getElementById('3').value)
+    num4 = parseInt(document.getElementById('4').value)
+    document.getElementById('resultado2').innerText = num3 - num4
+    console.log(num3 - num4)
 }
-function multiplicar(){
+function multiplicar() {
     let num5
     let num6
-    num5 = parseInt (document.getElementById('5').value)
-    num6 = parseInt (document.getElementById('6').value)
-    document.getElementById('resultado3').innerText = num5*num6
-    console.log(num5*num6)
+    num5 = parseInt(document.getElementById('5').value)
+    num6 = parseInt(document.getElementById('6').value)
+    document.getElementById('resultado3').innerText = num5 * num6
+    console.log(num5 * num6)
 }
-function area(){
+function area() {
     let num7
     let num8
-    num7 = parseInt (document.getElementById('7').value)
-    num8 = parseInt (document.getElementById('8').value)
-    document.getElementById('resultado4').innerText = num7*num8/2
-    console.log(num7*num8/2)
+    num7 = parseInt(document.getElementById('7').value)
+    num8 = parseInt(document.getElementById('8').value)
+    document.getElementById('resultado4').innerText = num7 * num8 / 2
+    console.log(num7 * num8 / 2)
 }
-function areaq(){
+function areaq() {
     let num9
-    num9 = parseInt (document.getElementById('9').value)
-    document.getElementById('resultado5').innerText = num9*num9
-    console.log(num9*num9)
+    num9 = parseInt(document.getElementById('9').value)
+    document.getElementById('resultado5').innerText = num9 * num9
+    console.log(num9 * num9)
 }
-function arear(){
+function arear() {
     let num10
     let num11
-    num10 = parseInt (document.getElementById('10').value)
-    num11 = parseInt (document.getElementById('11').value)
-    document.getElementById('resultado6').innerText = num10*num11
-    console.log(num10*num11)
+    num10 = parseInt(document.getElementById('10').value)
+    num11 = parseInt(document.getElementById('11').value)
+    document.getElementById('resultado6').innerText = num10 * num11
+    console.log(num10 * num11)
 }
-function areat(){
+function areat() {
     let num12
     let num13
     let num14
-    num12 = parseInt (document.getElementById('12').value)
-    num13 = parseInt (document.getElementById('13').value)
-    num14 = parseInt (document.getElementById('14').value)
-    document.getElementById('resultado7').innerText = (num12*num13) / 2
-    console.log(((num12+num13)*num14) / 2)
+    num12 = parseInt(document.getElementById('12').value)
+    num13 = parseInt(document.getElementById('13').value)
+    num14 = parseInt(document.getElementById('14').value)
+    document.getElementById('resultado7').innerText = (num12 * num13) / 2
+    console.log(((num12 + num13) * num14) / 2)
 }
-function areal(){
+function areal() {
     let num15
     let num16
-    num15 = parseInt (document.getElementById('15').value)
-    num16 = parseInt (document.getElementById('16').value)
-    document.getElementById('resultado8').innerText = (num15*num15)*(num16)
-    console.log((num15*num15)*(num16))
-   
+    num15 = parseInt(document.getElementById('15').value)
+    num16 = parseInt(document.getElementById('16').value)
+    document.getElementById('resultado8').innerText = (num15 * num16) / 2
+    console.log((num15 * num15) * (num16))
+
 }
-function areac(){
+function areac() {
     let num17
-    num17 = parseInt (document.getElementById('17').value)
-    document.getElementById('resultado9').innerText = (3.14)*(num17*num17)
-    console.log((3.14)*(num17*num17))
+    num17 = parseInt(document.getElementById('17').value)
+    document.getElementById('resultado9').innerText = (3.14) * (num17 * num17)
+    console.log((3.14) * (num17 * num17))
 }
 
-function areas(){
+function areas() {
     let num18
     let num19
-    num18 = parseInt(document.getElementById('18').value)
+    num18 = parseFloat(document.getElementById('18').value)
     num19 = parseInt(document.getElementById('19').value)
-    document.getElementById('resultado10').innerText = (num18 * 360) * 3.14 * (num19 * num19)
-    console.log((num18 * 360) * 3.14 * (num19 * num19))
+    document.getElementById('resultado10').innerText = (360 * num18) * 3.14 * (num19 ** 2)
+    console.log((360 * num18) * 3.14 * (num19 ** 2))
 }
 
-function areaCe(){
+function areaCo() {
     let num20
     let num21
-    num20 = parseInt(document.getElementById('20').value)
+    num20 = parseFloat(document.getElementById('20').value)
     num21 = parseInt(document.getElementById('21').value)
-    document.getElementById('resultado11').innerText = 3.14 * (num20 * num20) - (num21 * num21)
-    console.log(3.14 * (num20 * num20) - (num21 * num21))
+    document.getElementById('resultado11').innerText = 3.14 * ((num20 ** 2) - (num21 ** 2)) // **2 sinifica elevado a 2
+    console.log(3.14 * ((num20 ** 2) - (num21 ** 2)))
 }
 
-function areae(){
+function areae() {
     let num22
     let num23
     num22 = parseInt(document.getElementById('22').value)
@@ -97,20 +97,20 @@ function areae(){
     console.log(3.14 * num22 * num23)
 }
 
-function areap(){
+function areap() {
     let num24
     let num25
-    num24 = parseInt(document.getElementById('24').value) // Base
-    num25 = parseInt(document.getElementById('25').value) // Altura
+    num24 = parseInt(document.getElementById('24').value)
+    num25 = parseInt(document.getElementById('25').value)
     document.getElementById('resultado13').innerText = num24 * num25
     console.log(num24 * num25)
 }
 
-function areaPo(){
+function areaPo() {
     let num26
     let num27
-    num26 = parseInt(document.getElementById('26').value) // Semiperímetro
-    num27 = parseInt(document.getElementById('27').value) // Apótema
+    num26 = parseInt(document.getElementById('26').value)
+    num27 = parseInt(document.getElementById('27').value)
     document.getElementById('resultado14').innerText = num26 * num27
     console.log(num26 * num27)
 }
